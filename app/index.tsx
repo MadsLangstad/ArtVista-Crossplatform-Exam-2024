ad3590ef84db16c13b0c1eee11f08f301c63336a
MadsLangstad <langstad.dev@proton.me>
2024-11-04 14:15:14 +0100

    Update SplashScreen component: adjust layout, enhance welcome message, and add Enter button for navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,46 @@
 import { useEffect } from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 
 export default function SplashScreen() {
   const router = useRouter();
 
   useEffect(() => {
-    // Set a 3-second timer to navigate to the gallery screen
     const timer = setTimeout(() => {
       router.push("/(tabs)/");
-    }, 2000);
+    }, 10002000);
 
-    // Clear the timer if the component unmounts
     return () => clearTimeout(timer);
   }, [router]);
 
   return (
-    <View className="flex-1 justify-center items-center bg-black px-6">
+    <View className="flex top-32 justify-center items-center bg-black px-6">
       <Image
-        source={require("../assets/images/artivista-logo.png")}
-        className="w-80 h-80 mb-10"
+        source={require("../assets/images/artvista-logo.png")}
+        className="w-96 h-96"
         resizeMode="contain"
       />
 
-      <View className="flex gap-4">
-        <Text className="text-white text-4xl font-extrabold text-center">
-          Welcome to ArtiVista
+      <View className="flex gap-6 items-center">
+        <Text className="text-[#00BFFF] text-3xl font-extrabold text-center tracking-wider">
+          Welcome To ArtVista
         </Text>
-        <Text className="text-gray-300 text-lg text-center">
-          Discover a world of inspiration, curated by artists for art lovers.
-          Step into the future of digital art exhibitions.
+        <Text className="text-pink-500 text-lg font-semibold text-center leading-relaxed">
+          Step into a world of inspiration where digital art comes alive.
+          Curated by artists for art lovers, experience the future of art
+          exhibitions today.
         </Text>
+
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/(tabs)/");
+          }}
+          className="flex justify-center items-center border-2 border-[#00BFFF] w-28 rounded-lg mt-20"
+        >
+          <Text className="text-[#00BFFF] text-2xl font-semibold px-4">
+            Enter
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
